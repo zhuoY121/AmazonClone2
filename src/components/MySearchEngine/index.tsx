e6c4266a92bdf0cd5e335d00aa10efef2f9db26f
zhuoY121 <102482288+zhuoY121@users.noreply.github.com>
2022-05-20 13:35:27 -0400

    comment-searchEngine

--- src/components/MySearchEngine/index.tsx
+++ src/components/MySearchEngine/index.tsx
@@ -1,54 +1,59 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
-import {Text, Card, CardItem, Input, Item, Icon} from 'native-base';
+import {StyleSheet, SafeAreaView, Text} from 'react-native';
+// import {Text, Card, CardItem, Input, Item, Icon} from 'native-base';
 
-import SearchEngine from 'react-native-search-engine';
+// import SearchEngine from 'react-native-search-engine';
 
-const dataArray = ['Manizales+', 'Medellín|', 'Bogotá', 'Viena', 'Valencia'];
-const dataArrayInt = [
-  0.1, 0.2, 0.3, 0.4, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9,
-];
+// const dataArray = ['Manizales+', 'Medellín|', 'Bogotá', 'Viena', 'Valencia'];
+// const dataArrayInt = [
+//   0.1, 0.2, 0.3, 0.4, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9,
+// ];
 
-const customizeResult = properties => {
-  const {valueResult, element} = properties;
-  console.log('element>>>', element);
-  return (
-    <Card style={styles.containerCard}>
-      <CardItem style={styles.containerCardResult}>
-        <Text>{valueResult}</Text>
-      </CardItem>
-    </Card>
-  );
-};
-const customizeInput = properties => {
-  const {search, setSearch} = properties;
-  return (
-    <Item rounded style={styles.containerInput}>
-      <Input
-        style={styles.input}
-        value={search}
-        placeholder="email"
-        autoCorrect={false}
-        onChangeSearch={object => setSearch(object)}
-        autoCapitalize="words"
-        autoCompleteType="off"
-      />
-    </Item>
-  );
-};
+// const customizeResult = properties => {
+//   const {valueResult, element} = properties;
+//   console.log('element>>>', element);
+//   return (
+//     <Card style={styles.containerCard}>
+//       <CardItem style={styles.containerCardResult}>
+//         <Text>{valueResult}</Text>
+//       </CardItem>
+//     </Card>
+//   );
+// };
+// const customizeInput = properties => {
+//   const {search, setSearch} = properties;
+//   return (
+//     <Item rounded style={styles.containerInput}>
+//       <Input
+//         style={styles.input}
+//         value={search}
+//         placeholder="email"
+//         autoCorrect={false}
+//         onChangeSearch={object => setSearch(object)}
+//         autoCapitalize="words"
+//         autoCompleteType="off"
+//       />
+//     </Item>
+//   );
+// };
 
 const MySearchEngine = () => {
-  const [option, setOption] = useState();
-  console.log('option>>>', option);
   return (
-    <SearchEngine
-      data={dataArray}
-      searchKey={'name'}
-      onChangeSearch={text => setOption(text)}
-      buttonEnabled={true}
-      placeholder={'email'}
-    />
+    <SafeAreaView>
+      <Text>MySearchEngine</Text>
+    </SafeAreaView>
   );
+  // const [option, setOption] = useState();
+  // console.log('option>>>', option);
+  // return (
+  //   <SearchEngine
+  //     data={dataArray}
+  //     searchKey={'name'}
+  //     onChangeSearch={text => setOption(text)}
+  //     buttonEnabled={true}
+  //     placeholder={'email'}
+  //   />
+  // );
 };
 
 export default MySearchEngine;
